aaa66530064efeba15cabf8d09c6eeef9c66cc07
FallingPluie <wubin42@outlook.com>
2022-10-18 21:43:49 +0800

    调整文件结构

--- packages/model.py
+++ packages/model.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 class perceptron:
     def __init__(self, learn_rate:float = 1.0) -> None:
@@ -75,3 +76,63 @@ class perceptron:
                 b:偏置
         """
         return self.__w,self.__b
+
+class logistic:
+    def __init__(self,learn_rate:float = 1.0) -> None:
+        """
+        创建logistic回归模型, 学习率(learn_rate)默认为1, 应大于0
+        """
+        self.__w = np.array([0,0,0]) # w1,w2,b
+        self.learn_rate = learn_rate
+
+    def fx(self,w:np.ndarray,x:np.ndarray) -> np.ndarray:
+        """
+        logistic函数 g(w·x)
+        """
+        z = np.dot(w,x)
+        if z > 0:
+            return 1/(1+np.exp(-z))
+        else:
+            return np.exp(z)/(np.exp(z)+1) # 避免因exp(z)过大导致数据溢出
+
+    def __update(self,x:np.ndarray,y:float) -> None:
+        """
+        迭代公式, 选取损失函数为L(w)=sum((y-logistic(w·x))^2), 梯度下降法
+
+        Args:
+            x (np.ndarray): 单组坐标
+            y (float): 该组标签
+        """
+        a = np.array([x[0],x[1],1])
+        self.__w = self.__w+self.learn_rate*(y-self.fx(self.__w,a))*self.fx(self.__w,a)*(1-self.fx(self.__w,a))*a
+
+    def train(self,x:np.ndarray,y:np.ndarray,n:int = 50000) -> None:
+        """
+        训练模型
+
+        Args:
+            x (np.ndarray): 特征集, 形如 [(x1_1,x2_1),(x1_2,x2_2),……,(x1_n,x2_n)]
+            y (np.ndarray): 标签集, 形如 [y_1,y_2,y_3,……,y_n]
+            n(int): 训练次数, 默认为50000
+        """
+        if len(x) != len(y):
+            print("feature do not match with label in quantity")
+            return None
+        count = 0
+        index = list(range(len(x)))
+        while count < n:
+            random.shuffle(index)
+            for i in index:
+                self.__update(x[i],y[i])
+            count += 1
+            if count%2500 == 0: print(count) # 报数
+
+    def get(self) -> tuple[list, float]:
+        """
+        Returns:
+            tuple[np.ndarray,float]: (w,b)
+                w:权重
+                b:偏置
+        """
+        return [self.__w[0],self.__w[1]],self.__w[2]
+
--- perceptron.py
+++ perceptron.py
@@ -1,19 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import os,sys
-parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0,parentdir)
-import dataCreator as dc
-import model
+import packages.dataGenerator as dg
+import packages.model as model
 
-lst,tst = dc.cLinear(5)
-A1 = model.perceptron()
+d1 = dg.linear()
+lst,tst = d1.classify(5)
+a1 = model.perceptron()
 x = np.array([i[0] for i in lst])
 y = np.array([i[1] for i in lst])
 #x = np.array([(1,2),(1,4),(3,5),(6,5),(3,2),(5,4)])
 #y = np.array([1,1,1,-1,-1,-1])
-A1.train(x,y)
-w,b = A1.get()
+a1.train(x,y)
+w,b = a1.get()
 #print(w,b)
 #print("{0}\n{1}".format(x,y))
 
@@ -27,5 +25,7 @@ for i in range(len(x)):
     plt.scatter(x[i][0],x[i][1],marker = mark, color = Color)  # type: ignore
 xp = np.linspace(0,10)
 yp = - w[0]/w[1] * xp - b/w[1]
-plt.plot(xp,yp)
+#yd = xp * d1.w + d1.b
+plt.plot(xp,yp,color = "blue")
+#plt.plot(xp,yd,color = "black")
 plt.show()
